1a58764c44b74580dfadb489a6af83345c81cc6a
Zhaolong Wang <Zhaolongw@mtygroup.com>
2019-06-11 15:11:31 -0400

    day02

---
+++ python_100_days/day02.py
@@ -0,0 +1,18 @@
+
+a = int(input('a = '))
+b = int(input('b = '))
+print('%d / %d = %f' % (a, b, a / b))
+print('%d // %d = %d' % (a, b, a // b))
+print('%d %% %d = %d' % (a, b, a % b))
+print('%d ** %d = %d' % (a, b, a ** b))
+
+a = 100
+b = 12.345
+c = 1 + 5j
+d = 'hello, world'
+e = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
