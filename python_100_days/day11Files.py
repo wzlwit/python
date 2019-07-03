826c63875028729010bff6cbd19b119782d57ced
Zhaolong Wang <Zhaolongw@mtygroup.com>
2019-07-03 10:39:49 -0400

    day11

--- python_100_days/day11Files.py
+++
@@ -1,1 +0,0 @@
-https://github.com/jackfrued/Python-100-Days/blob/master/Day01-15/11.%E6%96%87%E4%BB%B6%E5%92%8C%E5%BC%82%E5%B8%B8.md
---
+++ python_100_days/day12Regex.py
@@ -0,0 +1,21 @@
+# https://github.com/jackfrued/Python-100-Days/blob/master/Day01-15/12.%E5%AD%97%E7%AC%A6%E4%B8%B2%E5%92%8C%E6%AD%A3%E5%88%99%E8%A1%A8%E8%BE%BE%E5%BC%8F.md
+import re
+
+
+def main():
+    username = input('请输入用户名: ')
+    qq = input('请输入QQ号: ')
+    # match函数的第一个参数是正则表达式字符串或正则表达式对象
+    # 第二个参数是要跟正则表达式做匹配的字符串对象
+    m1 = re.match(r'^[0-9a-zA-Z_]{6,20}$', username) # r: raw string
+    if not m1:
+        print('请输入有效的用户名.')
+    m2 = re.match(r'^[1-9]\d{4,11}$', qq)
+    if not m2:
+        print('请输入有效的QQ号.')
+    if m1 and m2:
+        print('你输入的信息是有效的!')
+
+
+if __name__ == '__main__':
+    main()
