db9459747df6be48b2f7dd4c9697d8519f79d4b0
Zhaolong Wang <zl.wangadfa@hotmail.com>
2020-01-16 11:32:29 -0500

    requests and decode('gbk')

--- web/Web_Requests.py
+++ web/Web_Requests.py
@@ -11,11 +11,13 @@ from html.parser import HTMLParser
 
 def main():
     parser = HTMLParser()
-    fName = "C:/Dev/WebInfo/temp.txt"
-    url = 'https://www.rona.ca/en'
+    fName = "C:/WebInfo/temp.html"
+    # url = 'https://www.rona.ca/en'
+    url = 'https://www.t66y.com/thread0806.php?fid=5'
     r = requests.get(url)
-    ctn = r.content.decode('utf-8')
-    print (ctn)
+    # ctn = r.content.decode('utf-8')
+    ctn = r.content.decode('gbk')
+    # print (ctn)
 
     open(fName, 'w', encoding="utf-8").write(ctn)
 
