6b4dc02674fe1dcdf25385e56657f4f3d6c7c585
Zhaolong Wang <Zhaolongw@mtygroup.com>
2020-01-24 15:23:56 -0500

    t66y fini

--- AV/t66y.py
+++ AV/t66y.py
@@ -8,14 +8,16 @@ from datetime import date
 
 fTemp = "C:/WebInfo/temp.html"
 f = "C:/WebInfo/t66y.csv"
+# f = "C:/WebInfo/t66y_test.csv"
 root = 'https://www.t66y.com'
+
 # date =date.today().strftime("%y-%m-%d")
 # date = date.today().isoformat()
-
 
 def animation():
     up = 1
     # for i in range(up,up+325):
+
     for i in range(up, up+2):
         num = i
         url = "{}/thread0806.php?fid=5&search=&page={}".format(root, num)
@@ -29,8 +31,7 @@ def animation():
         h3s = re.findall('<h3><a href="htm_data.*</h3>', ctn)
 
         # dates = re.findall('(?=<td><a href=\".*class=\"f10\">[ ])[0-9]{4}[/-]?[0-9]{2}[/-]?[0-9]{2}',ctn)
-        dates = re.findall(
-            'class="f10">\s[0-9]{4}[/-]?[0-9]{2}[/-]?[0-9]{2}', ctn)
+        dates = re.findall('class="f10">\s[0-9]{4}[/-]?[0-9]{2}[/-]?[0-9]{2}', ctn)
         # (?<=[ ][0-9:]{5})
 
         if len(dates) > len(h3s):
@@ -45,8 +46,7 @@ def animation():
             if suffix:
                 suffix = suffix.group(0)
                 # print(suffix)
-                title = h3[66:-9].replace('「', '').replace('」', "").split(
-                    ")")[-1].split("]")[-1].replace(suffix, '').strip()
+                title = h3[66:-9].replace('「', '').replace('」', "").split(")")[-1].split("]")[-1].replace(suffix, '').replace("[","").strip()
 
                 size = suffix.split("/")[-1]
                 mPos = size.find('M')
@@ -59,16 +59,62 @@ def animation():
                     hrefE = h3.split('"')[1]
                     href = "{}/{}".format(root, hrefE)
                     date = dates[n].split(">")[1].strip()
-                    strAdd = '"{}",{},=hyperlink("{}"),{},动画'.format(
-                        title, size, href, date)
+                    strAdd = '"{}",{},{},动画,=hyperlink("{}")'.format(title, size, date, href)
                     print(strAdd)
                     open(f, 'a', encoding="gbk").write('{}\n'.format(strAdd))
+        time.sleep(2)
 
+
+def foreign():
+    f = "C:/WebInfo/t66y_for.csv"
+    
+    up = 1
+    for i in range(up, up+2):
+        num = i
+        url = "{}/thread0806.php?fid=4&search=&page={}".format(root, num)
+        print(url)
+        r = requests.get(url)
+        ctn = r.content.decode('gbk')
+        open(fTemp, 'w', encoding='gbk').write(ctn)
+
+        h3s = re.findall('<h3><a href="htm_data.*</h3>', ctn)
+
+        # dates = re.findall('(?=<td><a href=\".*class=\"f10\">[ ])[0-9]{4}[/-]?[0-9]{2}[/-]?[0-9]{2}',ctn)
+        dates = re.findall('class="f10">\s[0-9]{4}[/-]?[0-9]{2}[/-]?[0-9]{2}', ctn)
+        # (?<=[ ][0-9:]{5})
+
+        if len(dates) > len(h3s):
+            dDif = len(dates)-len(h3s)
+            dates = dates[dDif:]
+
+        # print(len(h3s))
+        # print(len(dates))
+
+        for n, h3 in enumerate(h3s):
+            # print(h3)
+            prefix = re.search("FHD", h3)
+            if prefix:
+                prefix = prefix.group(0)
+                # print(prefix)
+                title = h3[66:-9].split("]")[1].replace(prefix, '').strip()
+                size = ''
+                # size = suffix.split("/")[-1]
+
+                titleL = open(f, 'r').read()
+
+                if not title in titleL:
+                    hrefE = h3.split('"')[1]
+                    href = "{}/{}".format(root, hrefE)
+                    date = dates[n].split(">")[1].strip()
+                    strAdd = '"{}",{},{},欧美,=hyperlink("{}")'.format(title, size, date, href)
+                    print(strAdd)
+                    open(f, 'a', encoding="gbk").write('{}\n'.format(strAdd))
         time.sleep(2)
 
 
 def main():
     animation()
+    foreign()
     print('Exit')
 
 
