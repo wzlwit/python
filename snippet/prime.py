011872433852c607c3a9c4ef18fd5fa1422a4a28
Zhaolong Wang <Zhaolongw@mtygroup.com>
2020-02-13 16:34:47 -0500

    prime

---
+++ snippet/prime.py
@@ -0,0 +1,32 @@
+import math
+
+
+def is_prime(n):
+
+    if n % 2 == 0:
+        return False
+
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+PRIMES = [
+    1116281,
+    1297337,
+    104395303,
+    472882027,
+    533000389,
+    817504243,
+    982451653,
+    112272535095293,
+    112582705942171,
+    112272535095293,
+    115280095190773,
+    115797848077099,
+    1099726899285419
+] * 5
+
+print(PRIMES)
